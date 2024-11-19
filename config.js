11d6d9735f9f424a95746bbeb00364636c443de6
ncamargop <n.camargop@uniandes.edu.co>
2024-11-19 09:38:58 -0500

    update mysql v2

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
-const mysql = require("mysql");
+const mysql = require("mysql2");
 require("dotenv").config();
 
 const urlDB = `mysql://${process.env.MYSQLUSER}:${process.env.MYSQLPASSWORD}@${process.env.MYSQLHOST}:${process.env.MYSQLPORT}/${process.env.MYSQLDATABASE}`;
--- server.js
+++ server.js
@@ -1,6 +1,5 @@
 const express = require("express");
-const app = express();
-const mysql = require("mysql2");
+const app = express();l2");
 const cors = require("cors");
 
 app.use(cors());
